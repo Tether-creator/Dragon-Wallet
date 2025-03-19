8078c758ab0e11bb1aa77f33c59e260682f7962a
Tether-creator <gruthzillions@gmail.com>
2025-03-19 16:51:31 +0100

    Delete src directory

--- src/app/layout.jsx
+++
@@ -1,9 +0,0 @@
-export const metadata = { title: 'Dragon Flash Wallet' };
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
