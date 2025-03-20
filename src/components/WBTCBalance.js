f523b1b572b0439d67a05af25d5d452b2e231676
Tether-creator <gruthzillions@gmail.com>
2025-03-20 16:13:54 +0100

    Update WBTCBalance.js

--- src/components/WBTCBalance.js
+++ src/components/WBTCBalance.js
@@ -24,7 +24,7 @@ export default function WBTCBalance({ provider, address }) {
 
   return (
     <div style={{ marginTop: '20px', fontSize: '1.2rem' }}>
-      <strong>WBTC Balance:</strong> {balance ? `${balance} ${symbol}` : 'Loading...'}
+      <strong>WBTC Balance:</strong> {balance ? `${balance} ${symbol}' : 'Loading...'}
     </div>
   );
 }
